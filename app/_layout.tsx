098afc2b85e3782351d2c4f3cdae18292b2c6a1d
freddymk <freddymk@Macbooks-MacBook-Air.local>
2025-12-12 15:20:20 +0400

    launch screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,22 +2,23 @@ import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
-import './../global.css'
+import './../global.css';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
-export const unstable_settings = {
-  anchor: '(tabs)',
-};
+// export const unstable_settings = {
+//   anchor: '(tabs)',
+// };
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack screenOptions={{ headerShown: false}}>
+        <Stack.Screen name="launch" />
       </Stack>
+
       <StatusBar style="auto" />
     </ThemeProvider>
   );
---
+++ app/launch.tsx
@@ -0,0 +1,55 @@
+import { useRouter } from "expo-router";
+import { View, Text, Image, Pressable } from "react-native";
+
+export default function LaunchScreen() {
+  const router = useRouter();
+
+  return (
+    <View className="flex-1 items-center justify-center bg-white p-6">
+      <View className="items-center mb-10">
+        <Image
+          source={require("@/assets/app/icon-blue.png")}
+          className="mb-4"
+          resizeMode="cover"
+        />
+      </View>
+
+      <Text
+        className="text-center text-[#4B4544] mb-12 max-w-xs"
+        // style={{ fontFamily: "PoppinsRegular", fontSize: 18 }}
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.
+      </Text>
+
+      <View className="w-full *max-w-xs gap-4">
+        <Pressable
+          className="bg-[#0088FF] py-4 w-[60%] self-center rounded-full items-center justify-center"
+          onPress={()=> router.navigate('/(auth)/login')} 
+        >
+          <Text
+            className="text-[#FFFFFF]"
+            // style={{ fontFamily: "PoppinsBold", fontSize: 15 }}
+          >
+            Log In
+          </Text>
+        </Pressable>
+
+        <Pressable
+          className="bg-[#F2F2F7] py-4 w-[60%] self-center rounded-full items-center justify-center *border border-[#0088FF]"
+          onPress={() => router.navigate('/(auth)/register')} 
+        >
+          <Text
+            className="text-[#0E3E3E]"
+            // style={{ fontFamily: "PoppinsBold", fontSize: 15 }}
+          >
+            Sign Up
+          </Text>
+        </Pressable>
+      </View>
+
+      <Pressable className="mt-6">
+        <Text className="text-[#093030] text-sm font-semibold">Forgot Password?</Text>
+      </Pressable>
+    </View>
+  );
+}
