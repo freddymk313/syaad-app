7564d56268a78747a004128a49bb93b270fe74fe
freddymk <freddymk@Macbooks-MacBook-Air.local>
2025-12-15 22:07:10 +0400

    adding screen

---
+++ app/(tabs)/transfert.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function transfert() {
+  return (
+    <View>
+      <Text>transfert</Text>
+    </View>
+  )
+}
