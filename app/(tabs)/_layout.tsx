865ff0624ee28528ed49596eff5e98f5bfa36846
freddymk <freddymk@Macbooks-MacBook-Air.local>
2025-12-15 22:01:13 +0400

    ok navbar started

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,63 +1,8 @@
 import { Tabs } from 'expo-router';
-import { Ionicons } from '@expo/vector-icons';
 import React from 'react';
 
 export default function TabsLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        headerShown: false,
-        tabBarActiveTintColor: '#0A66C2',
-        tabBarInactiveTintColor: 'gray',
-        tabBarStyle: { backgroundColor: 'white', height: 60 },
-      }}
-    >
-      <Tabs.Screen
-        name="home"
-        // options={{
-        //   title: 'Home',
-        //   tabBarIcon: ({ color, size }) => (
-        //     <Ionicons name="home-outline" size={size} color={color} />
-        //   ),
-        //   headerShown: false,
-        // }}
-      />
-      {/* <Tabs.Screen
-        name="network"
-        options={{
-          title: 'My Network',
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="people-outline" size={size} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="post"
-        options={{
-          title: 'Post',
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="add-circle-outline" size={size + 6} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="notifications"
-        options={{
-          title: 'Notifications',
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="notifications-outline" size={size} color={color} />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="jobs"
-        options={{
-          title: 'Jobs',
-          tabBarIcon: ({ color, size }) => (
-            <Ionicons name="briefcase-outline" size={size} color={color} />
-          ),
-        }}
-      /> */}
-    </Tabs>
+    <Tabs />
   );
 }
